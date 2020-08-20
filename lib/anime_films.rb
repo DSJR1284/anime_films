a8ab7a70e44b6a5f7e8f45cb5d930e7f97b9329a
DSJR1284 <DSJR1284@GMAIL.COM>
2020-08-20 19:45:20 -0400

    added new gem to cli

--- lib/anime_films.rb
+++ lib/anime_films.rb
@@ -1,6 +1,17 @@
-require "anime_films/version"
+require_relative "./anime_films/version"
+
+
+require "pry"
+require 'rest-client'
+require 'json'
+require 'colorize'
+
+require_relative "./anime_films/api"
+require_relative "./anime_films/films"
+require_relative "./anime_films/cli"
 
 module AnimeFilms
   class Error < StandardError; end
   # Your code goes here...
 end
+
